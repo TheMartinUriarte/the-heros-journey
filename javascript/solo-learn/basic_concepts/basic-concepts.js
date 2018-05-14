92d2d11a3a783327ad6b81ebfbd730a37edbcb4c
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-05-14 09:15:09 -0700

    [WIP] Completed Basic Concepts - Using Variables

--- javascript/solo-learn/basic_concepts/basic-concepts.js
+++ javascript/solo-learn/basic_concepts/basic-concepts.js
@@ -3,3 +3,6 @@
 // var x = 10;
 // Can't use vAr, must use let or const instead.
 const x = 10;
+
+document.write(x);
+// The above is an example of a statement using the variable we declared earlier.
