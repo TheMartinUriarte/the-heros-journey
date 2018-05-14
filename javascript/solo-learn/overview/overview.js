7b4632cfff2f293eceb87fe5d70f6b88634c3d8d
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-05-14 07:50:55 -0700

    [FIN] Solo learn - overview module completed

--- javascript/solo-learn/overview/overview.js
+++ javascript/solo-learn/overview/overview.js
@@ -1,1 +1,11 @@
-document.write("Hello world!");
+// Overview - "Creating Your First JavaScript"
+document.write("<h1>Hello world!</h1>");
+// It's possible to add formating to text
+
+// Overview - "Comments in JavaScript:"
+
+// Double slash allows for single line comments
+
+/* Slash stars allow for
+multi-line comments
+*/
