a505d618f05b515b64b473b807d20f97ae016866
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-06-12 16:44:25 -0700

    [FIN] Finished Solo Learn's JS Basic Concept Data Types module

---
+++ javascript/solo-learn/basic_concepts/basic-concepts_data-types.js
@@ -0,0 +1,18 @@
+// Fill in the blanks to declare a variable age and assign it the number 18:
+let age = 18;
+
+// A floating point number is allowed to have a decimal place
+
+/* You can use quotes inside a string, as long as they don't match the quotes surrounding the string.
+/* In order to create a string, we need to put the text inside... */
+const text = 'My name is "John"';
+
+// The backslash (\) escape character turns special characters into string characters.
+const sayHello = "Hello world! 'I am a JavaScript programmer.'";
+
+// \n = new line
+
+const s = 'I think "JavaScript is easy"';
+document.write(s);
+
+// The Boolean data type accepts only two values; true, and false.
