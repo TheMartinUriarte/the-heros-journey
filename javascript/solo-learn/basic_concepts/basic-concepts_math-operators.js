96e712aa348831bae7f1e96f8e06a13e433ae01c
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-06-12 16:50:08 -0700

    [FIN] Completed Solo Learn JS Basic Concepts Math Operators module

---
+++ javascript/solo-learn/basic_concepts/basic-concepts_math-operators.js
@@ -0,0 +1,23 @@
+let x = 10 + 5;
+document.write(x);
+
+// Outputs 15
+
+let test = 5 + 7;
+document.write(test);
+
+// Output 12
+
+let x = 10 * 5;
+document.write(x);
+
+// Outputs 50
+
+let x = 100 / 5;
+document.write(x);
+
+// Outputs 20
+
+let myVariable = 26 % 6;
+
+// myVariable equals 2
