1447ef56ed7d7ae0d157209251ce32380af3f193
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-06-12 17:36:54 -0700

    [FIN] Finished Solo Learn JS Basic Concepts String Operators module

---
+++ javascript/solo-learn/basic_concepts/basic-concepts_string-operators.js
@@ -0,0 +1,8 @@
+const mystring1 = 'I am learning';
+const mystring2 = 'JavaScript with SoloLearn.';
+document.write(mystring1 + mystring2);
+
+let x = '50';
+let y = '100';
+document.write(x + y);
+// Output 50100
