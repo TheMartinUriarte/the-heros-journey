7c6da66c5a3d547fcad42f2e9206834c18ec2ef0
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-06-12 17:18:56 -0700

    [FIN] Completed Solo Learn JS Basic Concepts Comparison Operators module

---
+++ javascript/solo-learn/basic_concepts/basic-concepts_comparison-operators.js
@@ -0,0 +1,12 @@
+// The equal to (==) operator checks whether the operands' values are equal.
+
+let num = 10;
+// num == 8 will return false
+
+val1 == val2; // are equal
+
+val1 != val2; // not equal
+
+val1 < val2; // less than
+
+val1 === val2; // Strictly equal
